70ad9a9e965f91233d4a0181df9fafa6b95d20a8
Magali FRANCOIS <magali.francois@labeo.fr>
2022-09-29 15:00:03 +0200

    start first test

---
+++ src/test/kotlin/RoverTest.kt
@@ -0,0 +1,12 @@
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.Test
+
+internal class RoverTest {
+
+    @Test
+    fun `should start with default position` () {
+        val rover = Rover();
+
+        Assertions
+    }
+}
